0796e48cd69657d64d6fe0c7447f1dbe8b80dcc1
dsr50 <butech01@outlook.com>
2013-11-10 21:21:57 -0600

    Update to JS, and added Notes

--- dragon.js
+++ dragon.js
@@ -4,7 +4,7 @@ switch(user) {
     case 'fight':
         var smart = prompt("Are you smart, mighty knight?").toLowerCase();
         var strong = prompt("Are you strong, courageous knight?").toLowerCase();
-            if(smart === 'yes' || strong == 'yes'){
+            if(smart === 'yes' || strong === 'yes'){
             alert("You are more than enough to slay this ugly demon! Choose you attack method!");
             }
             else {
@@ -15,14 +15,14 @@ switch(user) {
             alert("You swiped the demons head off with one swing.WOW! Pure show of amazement and talent! You defeated the mighty dragon!");
         }
         else if(attack1 === 'fist') {
-            alert("You courageous person you; You couldn't choose to use the swor...Wait...You actually killed that dragon with you bare hands...Can we be friends? Or at least not enemies...");
+            alert("You courageous person you; You couldn't choose to use the sword...Wait...You actually killed that dragon with you bare hands...Can we be friends? Or at least not enemies...");
         }
         else if(attack1 === 'shoot'){
             var gun = prompt("Choose you type of gun soldier!(Gatling or Scar)").toLowerCase();
             var gatling = true;
             var scar = true;
             if (gun === 'gatling' || gun === 'scar'){
-                alert("Guns have not yet been invinted you cheapskate. You could've at least used a sword but I guess you like the easy way out.");
+                alert("Guns have not yet been invented you cheapskate. You could've at least used a sword but I guess you like the easy way out.");
             }
         }
         else {
@@ -41,4 +41,36 @@ switch(user) {
         break;
     default:
         alert("Are you even speaking proper English my good knight? Try again and let's move on!");
+}
+
+var con1 = prompt("Fantastic! You have slayed the dragon and we must now continue our journey. Which direction would the mighty strong and brilliant knight traveler?(North, East, South, West)").toLowerCase();
+
+switch(con1) {
+    case 'north':
+        alert("To the North we shall journey!");
+            var rid1 = prompt("Time to answer the riddle and test your knowledge mighty knight! I Am The Beginning Of The End, And The End Of Time And Space. I Am Essential To Creation, And I Surround Every Place. What am I?").toLowerCase();
+                if (rid1 === 'the letter e' || 'e') {
+                        alert("Right you are, and also mighty intelligent! You see a glimmer of light coming from the ground, you glance down and see a beautiful diamond plated double edged sword!");
+                        var sword = prompt("Would you like to take or leave the glimmering sword?(Yes or No)").toLowerCase();
+                            if (sword === 'yes'){ 
+                                alert("Sword taken"); }
+                            else {
+                                alert("You chose to leave the sword hmm... I hope that doesn't come in handy later on."); }
+                            }
+                    else {
+                        alert("No, you answered incorrectly.");
+                     }
+        break;
+    case 'east':
+        alert("East it is!");
+            alert("Be careful great knight, I have always heard that bad things happen when settlers would travel East. Let's watch our backs and make sure that we do not become someone or somethings lunch!");
+                var choice1 = prompt("As you walk through the thick wooded forest, you begin to notice that the ground is far and deep, the grass is not green but red, and the tree's around you seem to be noticing you. You come upon two paths, and you notice that two roads split in the wood, which do you take? The road that more grassy and wanted wear, or the road less traveled by?(Grass, or Less)").toLowerCase();
+                    if (choice1 === grass) {
+                        alert("Some like not the road of trouble but the road of ease, your time will come in which you will endure the consequences of your decision."); }
+                        else {
+                            alert("Even though this will be a long and treacherous road, you will survive to live another day, and live to be the most courageous knight in the kingdom. You my dear friend, will live to see this decision prosper and will have riches and wealth unimaginable! You are a true knight!");
+                        }
+        break;
+    default:
+        alert("I'm sorry my knight, try again."); 
 }
